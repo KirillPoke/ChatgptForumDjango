973d849fc223aa8a2838ea9c24794347e1e48cfc
Kirill <kr6512592@gmail.com>
2023-07-09 20:09:23 +0300

    init migrations

---
+++ forum/forum/models.py
@@ -0,0 +1,28 @@
+from django.db.models import Model, AutoField, ForeignKey, CASCADE, SET_NULL, DateTimeField, CharField
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.db import models
+
+
+class User(AbstractBaseUser):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField(unique=True)
+    google_id = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Post(Model):
+    db_table = 'posts'
+    id = AutoField(primary_key=True)
+    author = ForeignKey(User, on_delete=SET_NULL, null=True)
+    title = CharField(max_length=255)
+    created_at = DateTimeField(auto_now_add=True)
+
+
+class Comment(Model):
+    db_table = 'comments'
+    id = AutoField(primary_key=True)
+    author = ForeignKey(User, on_delete=SET_NULL, null=True)
+    text = CharField(max_length=255)
+    post_id = ForeignKey(Post, on_delete=CASCADE)

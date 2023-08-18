6c0facf53adef8abe46f59953f9791171a51d268
Kirill <kr6512592@gmail.com>
2023-08-19 00:03:29 +0300

    fixed post serializer

--- django_server/subserializers/posts.py
+++ django_server/subserializers/posts.py
@@ -11,14 +11,12 @@ class PostSerializer(ModelSerializer):
     author = StringRelatedField()
     user_score = SerializerMethodField("get_user_score")
 
-    def get_user_score(self, comment):
+    def get_user_score(self, post):
         request = self.context.get("request")
         if request.user.is_authenticated:
             try:
-                comment_score = PostScore.objects.get(
-                    user=request.user, comment=comment
-                )
-                return 1 if comment_score.upvote else -1
+                post_score = PostScore.objects.get(user=request.user, post=post)
+                return 1 if post_score.upvote else -1
             except PostScore.DoesNotExist:
                 return 0
         else:
@@ -26,4 +24,4 @@ class PostSerializer(ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ["id", "author", "title", "created_at"]
+        fields = ["id", "author", "title", "created_at", "user_score"]

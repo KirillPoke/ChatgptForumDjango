b97f0ece8520b78e4ee687d714f9e95672d67443
Kirill <kr6512592@gmail.com>
2023-08-18 20:32:53 +0300

    Add score getting for comments

--- django_server/subserializers/comments.py
+++ django_server/subserializers/comments.py
@@ -2,17 +2,31 @@ from rest_framework.fields import ReadOnlyField, SerializerMethodField
 from rest_framework.relations import PrimaryKeyRelatedField, StringRelatedField
 from rest_framework.serializers import ModelSerializer
 
-from django_server.models import Post, Comment
+from django_server.models import Post, Comment, CommentScore
 
 
 class CommentSerializer(ModelSerializer):
     id = ReadOnlyField()
     post_id = PrimaryKeyRelatedField(queryset=Post.objects.all())
     author = StringRelatedField()
+    user_score = SerializerMethodField("get_user_score")
+
+    def get_user_score(self, comment):
+        request = self.context.get("request")
+        if request.user.is_authenticated:
+            try:
+                comment_score = CommentScore.objects.get(
+                    user=request.user, comment=comment
+                )
+                return 1 if comment_score.upvote else -1
+            except CommentScore.DoesNotExist:
+                return 0
+        else:
+            return 0
 
     class Meta:
         model = Comment
-        fields = ["id", "post_id", "author", "text", "is_prompt"]
+        fields = ["id", "post_id", "author", "text", "is_prompt", "user_score"]
 
 
 class CommentSerializerPost(CommentSerializer):

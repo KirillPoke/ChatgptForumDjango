0bd32e65b302db9f92ca0b6dce63c9bfb3c34e56
Kirill <kr6512592@gmail.com>
2023-09-03 22:33:07 +0300

    Revert "Revert "Rework auth WIP""
    This reverts commit fd5fd1387feb743a1672dbc932b81416099e2b64.

--- django_server/admin.py
+++ django_server/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from django_server.models import Comment, Post, User, CommentScore, PostScore, Tag
 
-registered_models = [Comment, Post, User, CommentScore, PostScore, Tag]
+
+class CustomUserAdmin(UserAdmin):
+    # Customize the UserAdmin class as needed
+    list_display = ("email", "is_staff")
+    list_filter = ("is_staff", "is_superuser", "groups")
+    search_fields = ("username", "email")
+    ordering = ("-id",)
+    fieldsets = ((None, {"fields": ("email", "password")}),)
+
+
+registered_models = [Comment, Post, CommentScore, PostScore, Tag]
 for model in registered_models:
     admin.site.register(model)
+admin.site.register(User, CustomUserAdmin)

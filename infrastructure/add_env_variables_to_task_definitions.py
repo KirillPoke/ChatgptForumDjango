a967745e47a6ee7d400524edf927c2a64c407caa
Kirill <kr6512592@gmail.com>
2023-09-23 13:53:48 +0300

    Add script

---
+++ infrastructure/add_env_variables_to_task_definitions.py
@@ -0,0 +1,17 @@
+import json
+import os
+
+if __name__ == "__main__":
+    google_env_vars = ["JWT_SECRET_KEY", "GOOGLE_CLIENT_ID", "GOOGLE_CLIENT_SECRET"]
+    db_env_vars = ["DB_USER", "DB_PASSWORD", "DB_HOST"]
+    openai_env_vars = ["OPENAI_API_KEY"]
+
+    environment_json = []
+    for env_var in [*google_env_vars, *db_env_vars, *openai_env_vars]:
+        env_dict = {"name": env_var, "value": os.environ.get(env_var)}
+        environment_json.append(env_dict)
+    with open("task-definition.json") as task_definition_file:
+        task_definition = json.load(task_definition_file)
+        task_definition["containerDefinitions"][0]["environment"] = environment_json
+    with open("task-definition.json", "w") as task_definition_file:
+        json.dump(task_definition, task_definition_file, indent=4)

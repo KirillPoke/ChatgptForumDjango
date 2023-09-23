77bfdf6a7307ce1a7e788389ba6627a3b60d448a
Kirill <kr6512592@gmail.com>
2023-09-23 12:17:21 +0300

    create cloud settings

---
+++ infrastructure/cloud_local_settings.py
@@ -0,0 +1,21 @@
+import os
+import openai
+
+JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
+JWT_ALGORITHM = "HS256"
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET")
+CORS_ORIGIN_ALLOW_ALL = True
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": "5432",
+    }
+}
+SESSION_COOKIE_SECURE = False
+
+openai.api_key = os.environ.get("OPENAI_API_KEY")

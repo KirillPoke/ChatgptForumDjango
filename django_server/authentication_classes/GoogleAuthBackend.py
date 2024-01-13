130149aae8fde000e6334c33a2b2fabd89b56aef
Kirill <kr6512592@gmail.com>
2024-01-13 15:48:42 +0200

    debug

--- django_server/authentication_classes/GoogleAuthBackend.py
+++ django_server/authentication_classes/GoogleAuthBackend.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.contrib.auth.backends import BaseBackend
 from google.oauth2 import id_token
 from google.auth.transport.requests import Request
@@ -12,19 +14,23 @@ def registrate_user(user_data):
 
 class GoogleAuthBackend(BaseBackend):
     def authenticate(self, request, token=None):
+        logging.info(f"got token, length: {len(token)}")
         if token:
             try:
                 user_data = id_token.verify_oauth2_token(
                     token, Request(), GOOGLE_CLIENT_ID
                 )
+                logging.info(f"email: {user_data.get('email', 'no email')}")
                 try:
                     user = User.objects.get(email=user_data["email"])
                 except User.DoesNotExist:
+                    logging.info("registrating user")
                     user = registrate_user(user_data)
                 return user
             except Exception:
                 return None
         else:
+            logging.info("no token")
             return None
 
     def get_user(self, user_id):

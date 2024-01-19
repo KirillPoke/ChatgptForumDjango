65cb0d4195f25ec34a7166c4b44c3675a5b0b4cd
Kirill <kr6512592@gmail.com>
2024-01-19 19:38:10 +0200

    Added paginator

--- django_server/subviews/posts.py
+++ django_server/subviews/posts.py
@@ -1,15 +1,21 @@
 from rest_framework.viewsets import ModelViewSet
 
 from django_server.models import Post
+from django_server.paginators.default import DefaultPagination
 from django_server.subserializers.posts import PostSerializer
 
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
+    pagination_class = DefaultPagination
+    serializer_class = PostSerializer
 
     def get_queryset(self):
         query_params = self.request.query_params.dict()
+        if "page" in query_params:
+            del query_params["page"]
         queryset = self.queryset.filter(**query_params)
         return queryset
 
-    serializer_class = PostSerializer
+    # def list(self, *args, **kwargs):
+    #     print(1)

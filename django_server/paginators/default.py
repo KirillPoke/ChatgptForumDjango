9ee397ed420c3917341146ac9f409fe36b32cfab
Kirill <kr6512592@gmail.com>
2024-02-02 01:18:07 +0200

    Made pagination optional

--- django_server/paginators/default.py
+++ django_server/paginators/default.py
@@ -5,3 +5,10 @@ class DefaultPagination(PageNumberPagination):
     page_size = 20
     page_size_query_param = "page_size"
     max_page_size = 1000
+
+    def paginate_queryset(self, queryset, request, view=None):
+        # if the page param is not provided, do not apply pagination
+        if request.query_params.get(self.page_query_param, None) is None:
+            return None
+
+        return super().paginate_queryset(queryset, request, view)

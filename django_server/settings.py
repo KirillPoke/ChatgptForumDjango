c9c3a3a66ae8a2b1eb644862df3a847bd839de65
Kirill <kr6512592@gmail.com>
2024-12-17 22:31:00 +0200

    Add auth0 creds

--- django_server/settings.py
+++ django_server/settings.py
@@ -189,9 +189,6 @@ Q_CLUSTER = {
     },
 }
 
-GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
-GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET")
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",

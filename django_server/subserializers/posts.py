144aef0a7656f8034d78bcf3489be70f4c2c9117
Kirill <kr6512592@gmail.com>
2024-12-20 14:44:32 +0200

    Remove tags

--- django_server/subserializers/posts.py
+++ django_server/subserializers/posts.py
@@ -1,8 +1,7 @@
 from rest_framework.fields import ReadOnlyField, SerializerMethodField, CharField
 from rest_framework.serializers import ModelSerializer
 
-from django_server.models import Post, Tag
-from django_server.subserializers.tags import TagStringRelatedField
+from django_server.models import Post
 
 
 # test
@@ -11,7 +10,6 @@ class PostSerializer(ModelSerializer):
     created_at = ReadOnlyField()
     author = CharField(source="author.name", read_only=True)
     total_score = SerializerMethodField("get_total_score", read_only=True)
-    tags = TagStringRelatedField(many=True, queryset=Tag.objects.all(), required=False)
     prompt_mode = CharField(max_length=255)
 
     def create(self, validated_data):
@@ -21,10 +19,6 @@ class PostSerializer(ModelSerializer):
         if tags:
             post.tags.set(tags)
         return post
-
-    # def to_representation(self, obj):
-    #     self.fields["author"] = StringRelatedField()
-    #     return super().to_representation(obj)
 
     def get_total_score(self, post):
         return post.total_score
@@ -38,13 +32,5 @@ class PostSerializer(ModelSerializer):
             "created_at",
             "total_score",
             "chat_role",
-            "tags",
             "prompt_mode",
         ]
-
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    #     if self.instance is not None and not hasattr(self.instance, '_prefetched_objects_cache'):
-    #         # Add select_related to optimize the query
-    #         if isinstance(self.instance, QuerySet):
-    #             self.instance = self.instance.select_related('author')
